50a1a724e90b586e15f9f19c849739256125a098
maeda <tankmaeda@gmail.com>
2018-08-14 13:22:38 +0900

    Add project files.

---
+++ LearnLoginView/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using LearnLoginView.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace LearnLoginView.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        private MainViewModel ViewModel => DataContext as MainViewModel;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
